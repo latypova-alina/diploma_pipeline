f4bab7abef9e580b0d7c4c1eb4002f27c35a7953
latypova-alina <alina.latypova@flatstack.com>
2019-04-19 22:58:53 +0300

    pick up Ner output folders

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import os
+import re
 import pdb
 
 DATA_FOLDER = "../../src/diploma/pipeline/data"
@@ -6,3 +7,8 @@ NER_DIR = "../../../NeuroNER-master"
 NER_OUT_DIR = "../../src/diploma/pipeline/output/new_dir"
 
 os.system("cd {}/src && python3.6 main.py --dataset_text_folder={} --output_folder={}".format(NER_DIR, DATA_FOLDER, NER_OUT_DIR))
+
+ner_output_folders = []
+
+for dirpath, dirnames, filenames in os.walk("/home/alina/src/diploma/pipeline/output/new_dir"):
+  ner_output_folders = ner_output_folders + [dirname for dirname in dirnames if re.compile("data_2019").match(dirname)]
